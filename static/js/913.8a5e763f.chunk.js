8c52aede683e7f72c962edb792f78ce418264801
turasova <134943646+turasova@users.noreply.github.com>
2024-01-19 20:43:59 +0000

    Deploying to gh-pages from @ turasova/goit-react-hw-08-phonebook@cc43126885b0861f42771dcba8e38d89b39261b6 🚀

---
+++ static/js/913.8a5e763f.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[913],{4913:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"hello"})}}}]);
+//# sourceMappingURL=913.8a5e763f.chunk.js.map
